67b6f223be0b97f936d2ed31fdc461097157edfc
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-02-18 23:03:28 +0100

    End code for lesson 107 "Named Parameters with @Query" - adding method BookRepository.findByTitleWithQueryNamed() with @Query annotation and named parameter and test method.

--- src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -27,6 +27,12 @@ public class BookRepositoryTest {
     BookRepository bookRepository;
 
     @Test
+    void testBookQueryNamed() {
+        Book book = bookRepository.findByTitleWithQueryNamed("Clean Code");
+        assertThat(book).isNotNull();
+    }
+
+    @Test
     void testBookQuery() {
         Book book = bookRepository.findBookByTitleWithQuery("Clean Code");
 
