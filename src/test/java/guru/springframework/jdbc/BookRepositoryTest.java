211330ea434e647785d589d440ab7488b655bb2e
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-02-18 22:50:45 +0100

    End code for lesson 106 "Declaring Queries Using @Query" - adding method BookRepository.findBookByTitleWithQuery() with @Query annotation and test method.

--- src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -27,6 +27,13 @@ public class BookRepositoryTest {
     BookRepository bookRepository;
 
     @Test
+    void testBookQuery() {
+        Book book = bookRepository.findBookByTitleWithQuery("Clean Code");
+
+        assertThat(book).isNotNull();
+    }
+
+    @Test
     void testBookFututre() throws ExecutionException, InterruptedException {
         Future<Book> bookFuture = bookRepository.queryByTitle("Clean Code");
 
