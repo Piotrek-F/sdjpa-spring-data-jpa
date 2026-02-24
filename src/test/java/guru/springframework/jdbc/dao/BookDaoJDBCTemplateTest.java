eea19c37d3daf6333ab93b3d10f2089338c2a0a5
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-02-24 15:39:45 +0100

    End code for lesson 116 "Find All Books Order By Title" - adding new method and implementation in BookDaoJDBCTemplate.findAllBooksSortByTitle() and tests.

--- src/test/java/guru/springframework/jdbc/dao/BookDaoJDBCTemplateTest.java
+++ src/test/java/guru/springframework/jdbc/dao/BookDaoJDBCTemplateTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -31,6 +32,16 @@ public class BookDaoJDBCTemplateTest {
     @BeforeEach
     void setUp() {
         bookDao = new BookDaoJDBCTemplate(jdbcTemplate);
+    }
+
+    @Test
+    void findAllBooksPage1_SortByTitle() {
+        List<Book> books = bookDao.findAllBooksSortByTitle(PageRequest.of(0, 10,
+                Sort.by(Sort.Order.desc("title"))));
+//                Sort.by(Sort.Direction.DESC, "title")));
+
+        assertThat(books).isNotNull();
+        assertThat(books.size()).isEqualTo(10);
     }
 
     @Test
