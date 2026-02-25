65c7e90e4a1bf2706ca34009a3092f3aedd67839
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-02-25 16:28:06 +0100

    End code for assignment 15 "Assignment - Find Author by Last Name" - adding new method AuthorDao.findAllAuthorsByLastName() and implementation in AuthorDaoJDBCTemplate and new test class AuthorDaoJDBCTemplateTest with tests.

---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorMapper.java
@@ -0,0 +1,22 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.lang.Nullable;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthorMapper implements RowMapper<Author> {
+
+    @Nullable
+    @Override
+    public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Author author = new Author();
+        author.setId(rs.getLong("id"));
+        author.setFirstName(rs.getString("first_name"));
+        author.setLastName(rs.getString("last_name"));
+
+        return author;
+    }
+}
